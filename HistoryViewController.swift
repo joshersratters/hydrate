496fb784a87771fb1bc1cab7a672036628faf7a9
joshersratters <joshratcliffe@me.com>
2015-05-14 21:54:11 +0100

    Add view controllers

---
+++ HistoryViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HistoryViewController.swift
+//  Hydrate
+//
+//  Created by Joshua Ratcliffe on 13/05/2015.
+//  Copyright (c) 2015 Joshua Ratcliffe. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+    
+    @IBAction func doneModal(sender: UIBarButtonItem) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ViewController.swift
+++ ViewController.swift
@@ -9,7 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
     
+    @IBOutlet weak var numberOfCupsLabel: UILabel!
+    @IBOutlet weak var cupsLabel: UILabel!
+    
+    @IBAction func addCup(sender: UIButton) {
+        numberOfCupsLabel.text = "2"
+        updateCupsLabel()
+        println("Button pressed")
+    }
     //MARK: - Actionsheet
     @IBAction func showActionSheet(sender: AnyObject) {
         let optionMenu = UIAlertController(title: "test", message: "this is a test", preferredStyle: UIAlertControllerStyle.ActionSheet)
@@ -26,9 +35,24 @@ class ViewController: UIViewController {
         self.presentViewController(optionMenu, animated: true, completion: nil)
     }
     
+    func updateCupsLabel() {
+        if numberOfCupsLabel.text! == "1" {
+            cupsLabel.text = "Cup"
+        } else {
+            cupsLabel.text = "Cups"
+        }
+    }
+    
+    func initialiseVariables() {
+        cupsLabel.text = "Cup"
+        numberOfCupsLabel.text = "1"
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        initialiseVariables()
+        updateCupsLabel()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
