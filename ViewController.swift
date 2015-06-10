5ab15925850d6293e84f2ab84c0c8f7195d0fde0
joshersratters <joshratcliffe@me.com>
2015-06-10 01:22:16 +0100

    Update number of cups label with core data & change app colour scheme

--- ViewController.swift
+++ ViewController.swift
@@ -12,7 +12,7 @@ import CoreData
 class ViewController: UIViewController {
     let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
     
-    var cups = [NSManagedObject]()
+    var cups = [Model]()
     
     @IBOutlet weak var numberOfCupsLabel: UILabel!
     @IBOutlet weak var cupsLabel: UILabel!
@@ -20,7 +20,7 @@ class ViewController: UIViewController {
     @IBAction func addCup(sender: UIButton) {
         
     }
-    //MARK: - Actionsheet
+    // Action Sheet
     @IBAction func showActionSheet(sender: AnyObject) {
         let optionMenu = UIAlertController(title: "Share", message: "Share today's progress with Twitter", preferredStyle: UIAlertControllerStyle.ActionSheet)
         let shareAction = UIAlertAction(title: "Share", style: UIAlertActionStyle.Default, handler: {(alert: UIAlertAction!) -> Void in
@@ -36,24 +36,61 @@ class ViewController: UIViewController {
         self.presentViewController(optionMenu, animated: true, completion: nil)
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        fetchCup()
+        updateCupsLabel()
+        // Do any additional setup after loading the view, typically from a nib.
+        //Reference app delegate
+        let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        //Reference to managed object context
+        let context: NSManagedObjectContext = appDelegate.managedObjectContext!
+        
+        //Reference to fetch request
+        let fetchrequest = NSFetchRequest(entityName: "Cup")
+
+    }
+    
+    func fetchCup() {
+        let fetchRequest = NSFetchRequest(entityName: "Cup")
+        
+        // Create a sort descriptor object that sorts on the "time"
+        // property of the Core Data object
+        let sortDescriptor = NSSortDescriptor(key: "time", ascending: false)
+        
+        // Set the list of sort descriptors in the fetch request,
+        // so it includes the sort descriptor
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        if let fetchResults = managedObjectContext!.executeFetchRequest(fetchRequest, error: nil) as? [Model] {
+            cups = fetchResults
+        }
+    }
+    
     func updateCupsLabel() {
+        numberOfCupsLabel.text! = cups.count.description
+        
         if numberOfCupsLabel.text! == "1" {
             cupsLabel.text = "Cup today"
         } else {
             cupsLabel.text = "Cups today"
         }
     }
-    
-    func initialiseCupsLabel() {
-        cupsLabel.text = "Cup"
-        numberOfCupsLabel.text = "1"
-    }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        initialiseCupsLabel()
-        updateCupsLabel()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        //Reference app delegate
+        let appDelegate: AppDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        
+        //Reference to managed object context
+        let context: NSManagedObjectContext = appDelegate.managedObjectContext!
+        
+        //Reference to fetch request
+        let fetchrequest = NSFetchRequest(entityName: "Cup")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
